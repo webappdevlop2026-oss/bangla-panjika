b20ff77a1023dc09c88284ad34c1babd459994e9
Your Name <you@example.com>
2026-08-19 11:49:17 +0530

    Add phone-frame preview for desktop

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"0cd610717bde95fd88343c64f81c11ba4e5c00
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3788245798" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2930417069" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
